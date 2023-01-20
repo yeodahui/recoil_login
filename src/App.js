a53937a8de5173a91ee1519350d174117b860e88
여다희 <yeodahui.dev@gmail.com>
2023-01-20 20:23:19 +0900

    localStorage token 유무에 따라 라우팅 처리

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Logout from "./pages/Logout";
 import UserInfo from "./pages/UserInfo";
 import NotFound from "./pages/NotFound";
 import "./styles.css";
+import { userAtom } from "./atoms/AuthAtom";
+import { useRecoilValue } from "recoil";
 
 function Main() {
+  const { token } = useRecoilValue(userAtom);
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />}></Route>
-        <Route path="/login" element={<Login />}></Route>
-        <Route path="/logout" element={<Logout />}></Route>
-        <Route path="/user" element={<UserInfo />}></Route>
+        <Route
+          path="/login"
+          element={token ? <Navigate to="/" /> : <Login />}
+        ></Route>
+        <Route
+          path="/logout"
+          element={token ? <Logout /> : <Navigate to="/" />}
+        ></Route>
+        <Route
+          path="/user"
+          element={token ? <UserInfo /> : <Navigate to="/login" />}
+        ></Route>
         <Route path="*" element={<NotFound />}></Route>
       </Routes>
     </BrowserRouter>
