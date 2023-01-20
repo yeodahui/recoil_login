3bd71123b7cb675e15cb1a490f2f9bcf7badfb38
여다희 <yeodahui.dev@gmail.com>
2023-01-20 20:22:45 +0900

    Login 후 user 정보 저장

---
+++ src/atoms/AuthAtom.js
@@ -0,0 +1,22 @@
+import { atom } from "recoil";
+
+const localStorageEffect =
+  (key) =>
+  ({ onSet }) => {
+    onSet((newValue) => {
+      localStorage.setItem(key, JSON.stringify(newValue.token));
+    });
+  };
+
+export const userAtom = atom({
+  key: "user",
+  default: {
+    id: null,
+    username: null,
+    firstName: null,
+    lastName: null,
+    image: null,
+    token: localStorage.getItem("testToken"),
+  },
+  effects: [localStorageEffect("testToken")],
+});
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,17 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import { userAtom } from "../atoms/AuthAtom";
 
 function Login() {
+  const URL = "https://dummyjson.com/auth/login";
+
   const [userId, setUserId] = useState("");
   const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const setUser = useSetRecoilState(userAtom);
+  const user = useRecoilValue(userAtom);
 
   const handleId = (e) => {
     setUserId(e.target.value);
@@ -14,6 +23,23 @@ function Login() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    try {
+      const res = await axios.post(URL, {
+        headers: {
+          "Content-type": "application/json",
+        },
+        username: userId,
+        password: password,
+      });
+      setUser(res.data);
+      console.log(user);
+
+      navigate("/");
+    } catch (error) {
+      console.error(error);
+    }
+    setUserId("");
+    setPassword("");
   };
 
   return (
